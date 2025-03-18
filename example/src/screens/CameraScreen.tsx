46900bd344b288608b04ff1a43bf9891bbb8f973
Nathan Nguyen <nltn@msn.com>
2025-03-18 23:39:16 +0700

    feat: useEventListener

--- example/src/screens/CameraScreen.tsx
+++ example/src/screens/CameraScreen.tsx
@@ -1,27 +1,40 @@
 import { useState } from 'react';
-import { Text, StyleSheet, Button, ScrollView } from 'react-native';
+import { Text, StyleSheet, Button, ScrollView, Alert } from 'react-native';
 import { useCameraController } from '../CameraControllerContext';
-import { CameraEvents } from '../../../src/CameraEvents';
-import type { CaptureSettings } from 'ricoh-camera-controller';
+import { type CaptureSettings, CameraEvents } from 'ricoh-camera-controller';
+import { useEventListener } from '../hooks/useEventListener';
 
 export const CameraScreen = () => {
   const [count, setCount] = useState(0);
   const [text, setText] = useState('Hello, World!');
   const [textLog, setTextLog] = useState('Log');
   const [textCaptureSettings, setTextCaptureSettings] = useState('Log');
+
   const camera = useCameraController();
-  camera?.on(CameraEvents.Connected, (data) => {
+
+  // #region Camera Event Handlers
+  const handleCameraConnected = (data: { model: string; datetime: string }) => {
     setText(`Connected`);
     setTextLog(`Model: ${data.model} Datetime: ${data.datetime}`);
-  });
+  };
 
-  camera?.on(CameraEvents.Disconnected, (_) => {
+  const handleCameraDisconnected = () => {
     setText('Disconnected');
-  });
+  };
 
-  camera?.on(CameraEvents.CaptureSettingsChanged, (data: CaptureSettings) => {
+  const handleCaptureSettingsChanged = (data: CaptureSettings) => {
     if (data) setTextCaptureSettings(data.av);
-  });
+  };
+  // #endregion
+
+  console.log(CameraEvents.Connected);
+  useEventListener(camera, CameraEvents.Connected, handleCameraConnected);
+  useEventListener(camera, CameraEvents.Disconnected, handleCameraDisconnected);
+  useEventListener(
+    camera,
+    CameraEvents.CaptureSettingsChanged,
+    handleCaptureSettingsChanged
+  );
 
   return (
     <ScrollView style={styles.container}>
@@ -44,6 +57,17 @@ export const CameraScreen = () => {
             .catch((_) => {
               setTextLog('error>>>>');
             });
+        }}
+      />
+      <Button
+        title="Num of Listeners"
+        onPress={() => {
+          const numListeners = camera
+            ?.eventNames()
+            .reduce((count, eventName) => {
+              return count + camera.listenerCount(eventName);
+            }, 0);
+          Alert.alert(`Num of listeners: ${numListeners}`);
         }}
       />
       <Text>{textCaptureSettings}</Text>
