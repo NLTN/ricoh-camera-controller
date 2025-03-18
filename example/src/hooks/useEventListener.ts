8086121ddaf79eb41e3bfef4f52f703e1881ea57
Nathan Nguyen <nltn@msn.com>
2025-03-18 23:36:32 +0700

    feat: add userEventLister hook for adding and removing listeners

---
+++ example/src/hooks/useEventListener.ts
@@ -0,0 +1,18 @@
+import EventEmitter from 'events';
+import { useEffect } from 'react';
+
+export const useEventListener = (
+  emitter: EventEmitter,
+  eventName: string | symbol,
+  listener: (arg: any) => void
+) => {
+  useEffect(() => {
+    // Add the listener
+    emitter.on(eventName, listener);
+
+    // Cleanup: Remove the listener
+    return () => {
+      emitter.off(eventName, listener);
+    };
+  }, [emitter, eventName, listener]); // Dependencies
+};
