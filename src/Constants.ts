00715030771ffa17d30b0f1ca3562e51b1688a5b
Nathan Nguyen <nltn@msn.com>
2025-03-23 09:54:34 +0700

    feat: add GR_COMMANDS enum

---
+++ src/Constants.ts
@@ -0,0 +1,16 @@
+export enum GR_COMMANDS {
+  REFRESH_DISPLAY = 'mode refresh',
+  BUTTON_PLAY = 'bplay',
+  BUTTON_ZOOM_IN = 'btele',
+  BUTTON_ZOOM_OUT = 'bwide',
+  BUTTON_UP = 'bup', // Button Macro
+  BUTTON_DOWN = 'bdown', // Button White Balance
+  BUTTON_LEFT = 'bleft', // Fn1
+  BUTTON_RIGHT = 'bright', // Flash
+  BUTTON_OK = 'bok',
+  BUTTON_DISP = 'bdisp',
+  BUTTON_FN2 = 'btrash',
+  METERING_MODE_MULTI = 'mpset=EXP_METERING METERING_MODE_MULTI',
+  METERING_MODE_CENTER = 'mpset=EXP_METERING METERING_MODE_CENTER',
+  METERING_MODE_SPOT = 'mpset=EXP_METERING METERING_MODE_SPOT',
+}
