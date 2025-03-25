128f718066be234cb98389fb8c8c6b96e649e3e5
Nathan Nguyen <nltn@msn.com>
2025-03-26 02:01:18 +0700

    feat: add commands for ADJ button, Focus mode, and LCD sleep

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,8 @@
 export enum GR_COMMANDS {
   REFRESH_DISPLAY = 'cmd=mode refresh',
+  BUTTON_ADJ_LEFT = 'cmd=badjleft',
+  BUTTON_ADJ_OK = 'cmd=badjok',
+  BUTTON_ADJ_RIGHT = 'cmd=badjright',
   BUTTON_PLAY = 'cmd=bplay',
   BUTTON_ZOOM_IN = 'cmd=btele',
   BUTTON_ZOOM_OUT = 'cmd=bwide',
@@ -10,7 +13,18 @@ export enum GR_COMMANDS {
   BUTTON_OK = 'cmd=bok',
   BUTTON_DISP = 'cmd=bdisp',
   BUTTON_FN2 = 'cmd=btrash',
+  FOCUS_MODE_MULTI = 'mpset=FOCUS FOCUS_MODE_MULTI',
+  FOCUS_MODE_SPOT = 'mpset=FOCUS FOCUS_MODE_SPOT',
+  FOCUS_MODE_PINPOINT = 'mpset=FOCUS FOCUS_MODE_PINPOINT',
+  FOCUS_MODE_TRACKING = 'mpset=FOCUS FOCUS_MODE_TRACKING',
+  FOCUS_MODE_MANUAL = 'mpset=FOCUS FOCUS_MODE_MANUAL',
+  FOCUS_MODE_SNAP = 'mpset=FOCUS FOCUS_MODE_SNAP',
+  FOCUS_MODE_INFINITY = 'mpset=FOCUS FOCUS_MODE_INFINITY',
   METERING_MODE_MULTI = 'mpset=EXP_METERING METERING_MODE_MULTI',
   METERING_MODE_CENTER = 'mpset=EXP_METERING METERING_MODE_CENTER',
   METERING_MODE_SPOT = 'mpset=EXP_METERING METERING_MODE_SPOT',
+  LENS_LOCK = 'pset=LENS_LOCK 1', // Retract and lock the lens
+  LENS_UNLOCK = 'pset=LENS_LOCK 0', // Unlock the lens
+  LCD_SLEEP_ON = 'cmd=lcd sleep on',
+  LCD_SLEEP_OFF = 'cmd=lcd sleep off',
 }
