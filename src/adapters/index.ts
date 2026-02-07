39beddca8452d371bdb8da30ada911316ddb3d44
Nathan Nguyen <nltn@msn.com>
2026-02-07 02:32:41 -0800

    feat(RCC-26): add GR IV adapter

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,3 +1,4 @@
 import GR2Adapter from './gr2/GR2Adapter';
 import GR3Adapter from './gr3/GR3Adapter';
-export { GR2Adapter, GR3Adapter };
+import GR4Adapter from './gr4/GR4Adapter';
+export { GR2Adapter, GR3Adapter, GR4Adapter };
